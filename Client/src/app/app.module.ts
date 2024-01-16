19334911213f37a4387b161a684d31f44c40816b
Krzysztof Rychlik <kmrychlik05@gmail.com>
2024-01-16 22:42:14 +0100

    Installed angular material

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularMaterialModule } from './shared/angular-material.module';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { HomeComponent } from './pages/home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularMaterialModule // all angular material modules in one place for readability
   ],
   providers: [],
   bootstrap: [AppComponent]
