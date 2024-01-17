03d4e7109c148f9798de209f364f066e53c51e1a
Krzysztof Rychlik <kmrychlik05@gmail.com>
2024-01-17 22:10:02 +0100

    Angular material components

--- Client/src/app/shared/angular-material.module.ts
+++ Client/src/app/shared/angular-material.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatGridListModule } from '@angular/material/grid-list';
 
 @NgModule({
   exports: [
     MatInputModule,
     MatButtonModule,
-    MatSlideToggleModule
+    MatSlideToggleModule,
+    MatGridListModule
   ],
 })
 export class AngularMaterialModule {}
